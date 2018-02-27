a235a9e0c4ded8ee30df4e893343b8b2dc8c69e1
Takumi <34294333+takumi34@users.noreply.github.com>
2018-02-27 18:57:50 +0900

    Update 31.py

--- chapter_4_MorphologicalAnalysis/31.py
+++ chapter_4_MorphologicalAnalysis/31.py
@@ -3,5 +3,5 @@
 import pandas as pd
 
 df = pd.read_csv('Desktop/neko.txt.mecab',names=('a','b','c','d','e','f'))
-tmp = list(df[df['d'].str.contains('動詞', na=False)]['a'])　#動詞の表層形をすべて抽出
+tmp = list(df[df['d'].str.contains('動詞-自立','動詞-非自立', na=False)]['a'])　#動詞の表層形をすべて抽出
 print(tmp)
