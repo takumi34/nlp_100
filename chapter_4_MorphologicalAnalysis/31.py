cf5b577361b6458f3b50178223dae48810b6bc48
Takumi <34294333+takumi34@users.noreply.github.com>
2018-02-27 18:43:47 +0900

    Create 31.py

---
+++ chapter_4_MorphologicalAnalysis/31.py
@@ -0,0 +1,7 @@
+#本問題はPandasの利用を想定していないようだが、Pandasを使った方が楽だと思い、利用する
+
+import pandas as pd
+
+df = pd.read_csv('Desktop/neko.txt.mecab',names=('a','b','c','d','e','f'))
+tmp = list(df[df['d'].str.contains('動詞', na=False)]['a'])　#動詞の表層形をすべて抽出
+print(tmp)
