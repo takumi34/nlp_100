c8d0ab1196253a5dcfff3c4bbf7485794f30ecaa
takumi34 <1shiranui@gmail.com>
2020-05-06 12:58:30 +0900

    add 21.py

---
+++ chapter_3/21.py
@@ -0,0 +1,11 @@
+from modules import ch2_func
+
+import re
+
+text = ch2_func.extract_uk_text()
+pattern = '\[\[Category:.*?\]\]'
+results = re.findall(pattern,text)
+
+for i in results:
+  print(i)
+
